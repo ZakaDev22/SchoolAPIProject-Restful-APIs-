0a4f4ee35b2d7cef0f7f49ad9b3624b798c82328
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-11-02 00:52:34 +0100

    Complete the 3 Other  Methos For People API Controller (GetByName - Add New - Update - Delete)

--- Controllers/PeopleAPIController.cs
+++ Controllers/PeopleAPIController.cs
@@ -12,7 +12,7 @@ namespace SchoolWebAPIApp.Controllers
         [HttpGet("GetAll", Name = "GetAllPeople")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<IEnumerable<PersonDTO>>> GetAll()
+        public async Task<ActionResult<IEnumerable<PersonDTO>>> GetAllAsync()
         {
             IEnumerable<PersonDTO> PeopleList = await clsPeople.GetAllAsync();
 
@@ -28,22 +28,144 @@ namespace SchoolWebAPIApp.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<PersonDTO>> GetByID(int ID)
+        public async Task<ActionResult<PersonDTO>> GetByIDAsync(int ID)
         {
             if (ID <= 0)
             {
                 return BadRequest($"Bad Request With ID = {ID}");
             }
 
-            PersonDTO person = await clsPeople.GetByIDAsync(ID);
+            clsPeople person = await clsPeople.GetByIDAsync(ID);
 
             if (person is null)
             {
                 return NotFound($"There Is No Person With ID {ID} ");
             }
 
-            return Ok(person);
+            return Ok(person.pDTO);
         }
 
+        [HttpGet("GetByFullName", Name = "GetByFullName")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<PersonDTO>> GetByNameAsync(string FirstName, string LastName)
+        {
+            if (string.IsNullOrEmpty(FirstName) || string.IsNullOrEmpty(LastName))
+            {
+                return BadRequest($"Bad Request With Full Name FirstName Or LastName");
+            }
+
+            clsPeople person = await clsPeople.GetByNameAsync(FirstName, LastName);
+
+            if (person is null)
+            {
+                return NotFound($"There Is No Person Name {FirstName} {LastName} ");
+            }
+
+            return Ok(person.pDTO);
+        }
+
+        [HttpPost("AddNewPerson", Name = "AddNewPerson")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<PersonDTO>> AddNewPersonAsync(PersonDTO personDTO)
+        {
+            if (personDTO is null)
+            {
+                return BadRequest("Person DTO In Null!");
+            }
+
+            if (string.IsNullOrEmpty(personDTO.FirstName) || string.IsNullOrEmpty(personDTO.LastName) || personDTO.SchoolID <= 0 || personDTO.AddressID <= 0)
+            {
+                return BadRequest(" Some Person DTO Properties Are Empty!");
+            }
+
+            clsPeople person = new clsPeople(personDTO, clsPeople.enMode.AddNew);
+
+            if (await person.SaveAsync())
+            {
+                personDTO.PersonID = person.PersonID;
+
+                return CreatedAtRoute("GetByID", new { Id = personDTO.PersonID }, personDTO);
+            }
+            else
+            {
+                return StatusCode(500, new { Message = "Error, Person Was Not Save." });
+            }
+
+        }
+
+        [HttpPut("UpdatePerson", Name = "UpdatePerson")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<PersonDTO>> UpdatePersonAsync(int ID, PersonDTO personDTO)
+        {
+            if (personDTO is null)
+            {
+                return BadRequest("Person DTO In Null!");
+            }
+
+            if (string.IsNullOrEmpty(personDTO.FirstName) || string.IsNullOrEmpty(personDTO.LastName) || personDTO.SchoolID <= 0 || personDTO.AddressID <= 0)
+            {
+                return BadRequest(" Some Person DTO Properties Are Empty!");
+            }
+
+            clsPeople person = await clsPeople.GetByIDAsync(ID);
+
+            if (person is null)
+            {
+                NotFound("Error, Person Was Not Found!");
+            }
+
+            person.FirstName = personDTO.FirstName;
+            person.LastName = personDTO.LastName;
+            person.DateOfBirth = personDTO.DateOfBirth;
+            person.Gender = personDTO.Gender;
+            person.SchoolID = personDTO.SchoolID;
+            person.AddressID = personDTO.AddressID;
+
+            if (await person.SaveAsync())
+            {
+                personDTO.PersonID = person.PersonID;
+
+                return Ok($"Person With ID {personDTO.PersonID} Have Ben Updated successfully.");
+            }
+            else
+            {
+                return StatusCode(500, new { Message = "Error, Person Was Not Updated." });
+            }
+
+        }
+
+        [HttpDelete("DeletePerson", Name = "DeletePerson")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<bool>> DeletePersonAsync(int ID)
+        {
+            if (ID <= 0)
+            {
+                return BadRequest($"Invalid ID : {ID} !!!");
+            }
+
+            clsPeople person = await clsPeople.GetByIDAsync(ID);
+
+            if (person is null)
+            {
+                return NotFound($"Not Person Have Ben Found With ID {ID}");
+            }
+
+            if (!await clsPeople.DeleteAsync(person.PersonID))
+            {
+                return StatusCode(500, new { Messgae = "Error, Person Was Not Deleted" });
+            }
+            else
+                return Ok($"Success, Person With ID {ID} Have Ben Deleted Successfully.");
+        }
     }
 }

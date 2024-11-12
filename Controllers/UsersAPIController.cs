ec4807a6297ab1ea0cb70ff39ec629cca355288e
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-11-12 17:43:40 +0100

    This Commit Is After I Create Tow New Methods In User APi Class (GetUserById ANd FindUserByUserNameAndPassword)

--- Controllers/UsersAPIController.cs
+++ Controllers/UsersAPIController.cs
@@ -21,5 +21,42 @@ namespace SchoolWebAPIApp.Controllers
 
             return Ok(users);
         }
+
+        [HttpGet("GetUserByIDAsync/{UserID}", Name = "GetUserByIDAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<FullUserDTO>>> GetUserByIDAsync(int UserID)
+        {
+            if (UserID <= 0)
+                return BadRequest($"Invalid User ID !");
+
+            var user = await clsUsers.GetUserByIdAsync(UserID);
+
+            if (user == null)
+                return NotFound($"No User With ID {UserID} Is Not Found!");
+
+
+            return Ok(user.fullUserDTO);
+        }
+
+        [HttpGet("FindUserByUserNameAndPasswordAsync/{UserName}/{Password}", Name = "FindUserByUserNameAndPasswordAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<FullUserDTO>>> FindUserByUserNameAndPasswordAsync(string UserName, string Password)
+        {
+            if (string.IsNullOrEmpty(UserName) || string.IsNullOrEmpty(Password))
+                return BadRequest($"Invalid User ID !");
+
+            var user = await clsUsers.AuthenticateUserAsync(UserName, Password);
+
+            if (user == null)
+                return NotFound($"No User With UserName {UserName} And Pasword {Password} Is Not Found!");
+
+
+            return Ok(user.fullUserDTO);
+        }
+
     }
 }

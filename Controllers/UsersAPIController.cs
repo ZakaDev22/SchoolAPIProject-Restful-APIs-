069312c4c0532de10152e37e90db1039dfc115c7
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-11-14 23:01:52 +0100

    AddNewUser And Delete User And IS UserExist API Methods

--- Controllers/UsersAPIController.cs
+++ Controllers/UsersAPIController.cs
@@ -58,5 +58,74 @@ namespace SchoolWebAPIApp.Controllers
             return Ok(user.fullUserDTO);
         }
 
+        [HttpDelete("DeleteUserAsync/{UserID}", Name = "DeleteUserAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<bool>>> DeleteUserAsync(int UserID)
+        {
+            if (UserID <= 0)
+                return BadRequest($"Invalid User ID !");
+
+            var user = await clsUsers.GetUserByIdAsync(UserID);
+
+            if (user == null)
+                return NotFound($"No User With ID {UserID} Is Not Found!");
+
+            if (await clsUsers.DeleteUserAsync(UserID))
+            {
+                return Ok($"User With ID {UserID} Was Successfully Deleted.");
+            }
+            else
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error, Something Went Wrong In the Server, Please Try Again");
+        }
+
+        [HttpGet("IsUserExistAsync/{UserID}", Name = "IsUserExistAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<bool>>> IsUserExistAsync(int UserID)
+        {
+            if (UserID <= 0)
+                return BadRequest($"Invalid User ID !");
+
+            var user = await clsUsers.GetUserByIdAsync(UserID);
+
+            if (user == null)
+                return NotFound($"{false}, No User With ID {UserID}, User Is Not Found!");
+
+
+            return Ok(true);
+        }
+
+        [HttpPost("AddNewUser", Name = "AddNewUser")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<FullUserDTO>> AddNewUserAsync(FullUserDTO userDTO)
+        {
+            if (userDTO == null)
+            {
+                return BadRequest("user DTO Is Null!");
+            }
+
+            if (userDTO.PersonID <= 0 || userDTO.Permissions < 0 || userDTO.AddedByUserID <= 0 || string.IsNullOrEmpty(userDTO.Password))
+            {
+                return BadRequest(" Some User DTO Properties Are Empty!");
+            }
+
+            clsUsers user = new clsUsers(clsUsers.enMode.AddNew, userDTO);
+
+            if (await user.SaveAsync())
+            {
+                return CreatedAtRoute("GetByID", new { Id = user.userID }, user.UserDTO);
+            }
+            else
+            {
+                return StatusCode(500, new { Message = "Error, Person Was Not Save." });
+            }
+
+        }
     }
 }

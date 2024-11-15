87114965ce9dea5a0fc00cff47a822a7fa407fd4
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-11-15 18:32:30 +0100

    Apply Student First API Method (GetAll)

--- Controllers/StudentsAPIController.cs
+++ Controllers/StudentsAPIController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SchoolAPiDataAccessLayer;
+using SchoolBusinessLayer;
 
 namespace SchoolWebAPIApp.Controllers
 {
@@ -7,6 +9,18 @@ namespace SchoolWebAPIApp.Controllers
     public class StudentsAPIController : ControllerBase
     {
 
+        [HttpGet("GetAllStudents", Name = "GetAllStudents")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<studentDTO>>> GetAllStudents()
+        {
+            var users = await clsStudents.GetAllAsync();
+
+            if (users == null)
+                return NotFound("There Is No Data!");
+
+            return Ok(users);
+        }
 
 
     }

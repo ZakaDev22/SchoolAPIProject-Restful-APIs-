55bebcf66e2eef302ad95d9710e2e5f822aad955
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-11-21 00:09:22 +0100

    Finishing Subjects Data Access Methods (Crud + Tow Find Methods By ID - By Code)

---
+++ Controllers/SubjectsAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolWebAPIApp.Controllers
+{
+    [Route("api/SubjectsAPI")]
+    [ApiController]
+    public class SubjectsAPIController : ControllerBase
+    {
+        // Finishing The Data Access Methods
+    }
+}

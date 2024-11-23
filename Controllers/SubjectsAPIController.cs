7faaec5b7c140275046a79f7e5e39764a785e05e
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-11-23 22:23:47 +0100

    Finishing The Subject APIs Methods (Delete - Is Existe - AddNew - Update)

--- Controllers/SubjectsAPIController.cs
+++ Controllers/SubjectsAPIController.cs
@@ -57,5 +57,113 @@ namespace SchoolWebAPIApp.Controllers
 
             return Ok(subject.sbjDTO);
         }
+
+        [HttpGet("IsSubjectExistsByID/{ID}", Name = "IsSubjectExistsByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<bool>>> IsSubjectExistsByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            var IsExists = await clsSubjects.IsExistsAsync(ID);
+
+            if (!IsExists)
+                return NotFound($"No subject With ID {ID} Has Ben  Found!");
+
+
+            return Ok(IsExists);
+        }
+
+        [HttpDelete("DeleteSubjectByID/{ID}", Name = "DeleteSubjectByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<bool>>> DeleteSubjectByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            var IsExists = await clsSubjects.GetByIDAsync(ID);
+
+            if (IsExists is null)
+                return NotFound($"No Subject With ID {ID} Has Ben  Found!");
+
+
+            if (await clsSubjects.DeleteAsync(ID))
+                return Ok($"Success, Subject With ID {ID} Has Ben Deleted.");
+            else
+                return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        [HttpPost("AddNewSubject", Name = "AddNewSubject")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<staffDTO>> AddNewSubjectAsync(subjectDTO sDTO)
+        {
+            if (sDTO == null)
+            {
+                return BadRequest("DTO Is Null!");
+            }
+
+            if (sDTO.SchoolID <= 0 || string.IsNullOrEmpty(sDTO.SubjectName) || string.IsNullOrEmpty(sDTO.SubjectCode))
+            {
+                return BadRequest(" Some DTO Properties Are Empty!");
+            }
+
+            var subject = new clsSubjects(sDTO, clsSubjects.enMode.AddNew);
+
+            if (await subject.SaveAsync())
+            {
+                return CreatedAtRoute("GetSubjectByID", new { ID = subject.SubjectID }, subject.sbjDTO);
+            }
+            else
+            {
+                return StatusCode(500, new { Message = "Error, Subject Was Not Save." });
+            }
+
+        }
+
+        [HttpPut("UpdateSubject/{ID}", Name = "UpdateSubject")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<staffDTO>> UpdateSubjectAsync(int ID, subjectDTO sDTO)
+        {
+            if (sDTO == null)
+            {
+                return BadRequest("DTO Is Null!");
+            }
+
+            if (sDTO.SchoolID <= 0 || string.IsNullOrEmpty(sDTO.SubjectName) || string.IsNullOrEmpty(sDTO.SubjectCode))
+            {
+                return BadRequest(" Some DTO Properties Are Empty!");
+            }
+
+            var subject = await clsSubjects.GetByIDAsync(ID);
+
+            if (subject == null)
+                return NotFound($"No subject With {ID} Have Ben Found");
+
+            subject.SubjectName = sDTO.SubjectName;
+            subject.SubjectCode = sDTO.SubjectCode;
+            subject.SchoolID = sDTO.SchoolID;
+
+
+            if (await subject.SaveAsync())
+            {
+                return Ok($"Success, Subject With ID {subject.SubjectID} Has Ben Updated Successfully.");
+            }
+
+            else
+            {
+                return StatusCode(500, new { Message = "Error, Subject Was Not Save." });
+            }
+
+        }
     }
 }

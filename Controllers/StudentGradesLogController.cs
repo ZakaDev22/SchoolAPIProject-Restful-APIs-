ad1240ec105a5579747905b2829b498f4934af42
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-11-23 23:54:48 +0100

    Finishing Student Grade Log APIs (Delete - Add New - Update) WIth The Data Access Bugs ✅

--- Controllers/StudentGradesLogController.cs
+++ Controllers/StudentGradesLogController.cs
@@ -59,5 +59,96 @@ namespace SchoolWebAPIApp.Controllers
             return Ok(IsExists);
         }
 
+
+        [HttpDelete("DeleteStudentGradeLogByID/{ID}", Name = "DeleteStudentGradeLogByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<bool>>> DeleteStudentGradeLogByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            var IsExists = await clsStudentGradesLog.GetByIDAsync(ID);
+
+            if (IsExists is null)
+                return NotFound($"No Student Grade Log With ID {ID} Has Ben  Found!");
+
+
+            if (await clsStudentGradesLog.DeleteAsync(ID))
+                return Ok($"Success, Student Grade Log With ID {ID} Has Ben Deleted.");
+            else
+                return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        [HttpPost("AddNewStudentGradeLog", Name = "AddNewStudentGradeLog")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<staffDTO>> AddNewStudentGradeLogAsync(sgLogDTO sDTO)
+        {
+            if (sDTO == null)
+            {
+                return BadRequest("DTO Is Null!");
+            }
+
+            if (sDTO.StudentID <= 0 || sDTO.SubjectID <= 0 || sDTO.Grade < 0)
+            {
+                return BadRequest(" Some DTO Properties Are Empty!");
+            }
+
+            var sgLog = new clsStudentGradesLog(sDTO, clsStudentGradesLog.enMode.AddNew);
+
+            if (await sgLog.SaveAsync())
+            {
+                return CreatedAtRoute("GetStudentGradeLogByID", new { ID = sgLog.LogID }, sgLog.sgLogDTO);
+            }
+            else
+            {
+                return StatusCode(500, new { Message = "Error, Subject Was Not Save." });
+            }
+
+        }
+
+        [HttpPut("UpdateStudentGradeLog/{ID}", Name = "UpdateStudentGradeLog")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<staffDTO>> UpdateStudentGradeLogAsync(int ID, sgLogDTO sDTO)
+        {
+            if (sDTO == null)
+            {
+                return BadRequest("DTO Is Null!");
+            }
+
+            if (sDTO.StudentID <= 0 || sDTO.SubjectID <= 0 || sDTO.Grade < 0)
+            {
+                return BadRequest(" Some DTO Properties Are Empty!");
+            }
+
+            var sgLog = await clsStudentGradesLog.GetByIDAsync(ID);
+
+            if (sgLog is null)
+                return NotFound($"No sgLog With {ID} Have Ben Found");
+
+            sgLog.StudentID = sDTO.StudentID;
+            sgLog.SubjectID = sDTO.SubjectID;
+            sgLog.Grade = sDTO.Grade;
+            sgLog.Comments = sDTO.Comments ?? string.Empty;
+
+
+            if (await sgLog.SaveAsync())
+            {
+                return Ok($"Success,  Grade Log With ID {sgLog.LogID} Has Ben Updated Successfully.");
+            }
+
+            else
+            {
+                return StatusCode(500, new { Message = "Error, Grade Log Was Not Save." });
+            }
+
+        }
     }
 }

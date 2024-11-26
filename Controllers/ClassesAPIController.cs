0117aaa57deca5e3b20e450f4f780a2f7ee45e2a
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-11-26 18:56:46 +0100

    Finishing Three APIs For Classes (Get All - Get By Id - Is Exists)

---
+++ Controllers/ClassesAPIController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SchoolAPiDataAccessLayer;
+using SchoolBusinessLayer;
+
+namespace SchoolWebAPIApp.Controllers
+{
+    [Route("api/ClassesAPI")]
+    [ApiController]
+    public class ClassesAPIController : ControllerBase
+    {
+        [HttpGet("GetAllClasses", Name = "GetAllClasses")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<classDTO>>> GetAllClassesAsync()
+        {
+            var classes = await clsClasses.GetAllAsync();
+
+            if (classes.IsNullOrEmpty())
+                return NotFound("There Is No Data!");
+
+            return Ok(classes);
+        }
+
+        [HttpGet("GetClassByID/{ID}", Name = "GetClassByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<classDTO>>> GetClassByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            var @class = await clsClasses.GetByIDAsync(ID);
+
+            if (@class == null)
+                return NotFound($"No class With ID {ID} Is Not Found!");
+
+
+            return Ok(@class.classDTO);
+        }
+
+        [HttpGet("IsClassExistsByID/{ID}", Name = "IsClassExistsByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<bool>>> IsClassExistsByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            if (!await clsClasses.IsExistsAsync(ID))
+                return NotFound($"No class With ID {ID} Has Ben  Found!");
+
+
+            return Ok(true);
+        }
+    }
+}

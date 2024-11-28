b2670bbc71bb90a389793474eaadeb0add981ba0
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-11-28 23:45:52 +0100

    Starting On User Register Classes ( dataAccess - Buseniss And The API Part)

---
+++ Controllers/UserRegisterAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolWebAPIApp.Controllers
+{
+    [Route("api/UserRegisterAPI")]
+    [ApiController]
+    public class UserRegisterAPIController : ControllerBase
+    {
+
+    }
+}

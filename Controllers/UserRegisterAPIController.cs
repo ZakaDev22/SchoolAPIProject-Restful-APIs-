2f8e88f5258dc5d3282c488dc4c8d0ac279f90dc
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-11-29 01:31:22 +0100

    Get All And Get By ID And Update Methods Don Successfully. -I have To Fix Add New Method With The Internal SQL Server Exception Problem 😐😐😐 - Delete Method In Progress

--- Controllers/UserRegisterAPIController.cs
+++ Controllers/UserRegisterAPIController.cs
@@ -43,5 +43,90 @@ namespace SchoolWebAPIApp.Controllers
             return Ok(register.RegisterDTO);
         }
 
+
+        [HttpDelete("DeleteRegisterByID/{ID}", Name = "DeleteRegisterByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<bool>>> DeleteRegisterByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            var IsExists = await clsUserRegister.GetByIDAsync(ID);
+
+            if (IsExists is null)
+                return NotFound($"No Register With ID {ID} Has Ben  Found!");
+
+
+            if (await clsUserRegister.DeleteAsync(ID))
+                return Ok($"Success, Register With ID {ID} Has Ben Deleted.");
+            else
+                return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        [HttpPost("AddNewRegister", Name = "AddNewRegister")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<subjectDTO>> AddNewRegisterAsync(userRegisterDTO sDTO)
+        {
+            if (sDTO == null)
+            {
+                return BadRequest("DTO Is Null!");
+            }
+
+            if (sDTO.UserID <= 0 || string.IsNullOrEmpty(sDTO.IPAddress))
+            {
+                return BadRequest(" Some DTO Properties Are Empty!");
+            }
+
+            var register = new clsUserRegister(sDTO, clsUserRegister.enMode.AddNew);
+
+            if (await register.SaveAsync())
+            {
+                return CreatedAtRoute("GetRegisterByID", new { ID = register.RegisterID }, register.RegisterDTO);
+            }
+            else
+            {
+                return StatusCode(500, new { Message = "Error, Register Was Not Save." });
+            }
+
+        }
+
+        [HttpPut("UpdateRegister/{ID}", Name = "UpdateRegister")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<userRegisterDTO>> UpdateRegisterAsync(int ID)
+        {
+            if (ID <= 0)
+            {
+                return BadRequest("Invalid ID!");
+            }
+
+            var register = await clsUserRegister.GetByIDAsync(ID);
+
+            if (register is null)
+                return NotFound($"No register With {ID} Have Ben Found");
+
+
+            // if We Need To Update Register We Only Need To Call The Save Method After We Found The Record We Need 
+            // Because After We Found The Record The Mode Was Set To Update Mode
+            // And At The End We Only Need To Send The Record ID To The Update Method 
+            // The Stored Procedure Will take care of Updating The Logout Field To The Time The Current User Is Logout
+            if (await register.SaveAsync())
+            {
+                return Ok($"Success, Register With ID {register.RegisterID} Has Ben Updated Successfully.");
+            }
+
+            else
+            {
+                return StatusCode(500, new { Message = "Error, Subject Was Not Save." });
+            }
+
+        }
     }
 }

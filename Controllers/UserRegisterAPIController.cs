36809c0c749ce6129348254397bc0955945bb4ab
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-11-29 01:01:41 +0100

    - Alter The Yser Register Classes ---> Get Methods In The Data Access With a Null Problem From The Retrived data in  The SQL Server (Status 500 😐)

--- Controllers/UserRegisterAPIController.cs
+++ Controllers/UserRegisterAPIController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SchoolAPiDataAccessLayer;
+using SchoolBusinessLayer;
 
 namespace SchoolWebAPIApp.Controllers
 {
@@ -8,5 +11,37 @@ namespace SchoolWebAPIApp.Controllers
     {
         // Complete Register Data Access And Logic Layer 
         // Alter The Get Methods In All The Logic Layer Classes With Short Hand If Logic For Clear And simple Readable Code
+
+        [HttpGet("GetAllRegisters", Name = "GetAllRegisters")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<userRegisterDTO>>> GetAllRegistersAsync()
+        {
+            var regesters = await clsUserRegister.GetAllAsync();
+
+            if (regesters.IsNullOrEmpty())
+                return NotFound("There Is No Data!");
+
+            return Ok(regesters);
+        }
+
+        [HttpGet("GetRegisterByID/{ID}", Name = "GetRegisterByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<userRegisterDTO>>> GetByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            var register = await clsUserRegister.GetByIDAsync(ID);
+
+            if (register == null)
+                return NotFound($"No Subject With ID {ID} Is Not Found!");
+
+
+            return Ok(register.RegisterDTO);
+        }
+
     }
 }

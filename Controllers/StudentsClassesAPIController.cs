e18977d15d300a402860142fcfa8a309710b5679
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-02 23:57:12 +0100

    Add New Class

---
+++ Controllers/StudentsClassesAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolWebAPIApp.Controllers
+{
+    [Route("api/StudentsClassesAPI")]
+    [ApiController]
+    public class StudentsClassesAPIController : ControllerBase
+    {
+
+    }
+}

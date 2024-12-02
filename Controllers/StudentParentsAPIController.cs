f3e26f40a2e883ad53ae91464d3f669e818eb7f1
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-03 00:53:05 +0100

    Create Students Parents Branches

---
+++ Controllers/StudentParentsAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolWebAPIApp.Controllers
+{
+    [Route("api/StudentParentsAPI")]
+    [ApiController]
+    public class StudentParentsAPIController : ControllerBase
+    {
+
+    }
+}

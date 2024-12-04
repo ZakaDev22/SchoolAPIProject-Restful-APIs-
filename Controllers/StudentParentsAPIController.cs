c24f0bb1fb88a1df21e3afeff6a6bac17f2a340a
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-04 01:40:04 +0100

    Finishing Students Parents Data Access Methods Add Get All Mehtod In Logic Layer Add Get All In The API Layer (Test Get All Done Successfully.)

--- Controllers/StudentParentsAPIController.cs
+++ Controllers/StudentParentsAPIController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SchoolAPiDataAccessLayer;
+using SchoolBusinessLayer;
 
 namespace SchoolWebAPIApp.Controllers
 {
@@ -6,6 +9,17 @@ namespace SchoolWebAPIApp.Controllers
     [ApiController]
     public class StudentParentsAPIController : ControllerBase
     {
+        [HttpGet("GetAllStudentsParents", Name = "GetAllStudentsParents")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<studentClassDTO>>> GetAllStudentsParentsAsync()
+        {
+            var studentsParents = await clsStudentParents.GetAllAsync();
 
+            if (studentsParents.IsNullOrEmpty())
+                return NotFound("There Is No Data!");
+
+            return Ok(studentsParents);
+        }
     }
 }

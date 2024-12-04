304e21ba8f15e1e1cdc6342ab6393ce1645ec719
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-04 20:27:33 +0100

    Complete The Logic Layer Add Three API Methods (Ge By ID - Is Exists - Delete)

--- Controllers/StudentParentsAPIController.cs
+++ Controllers/StudentParentsAPIController.cs
@@ -21,5 +21,59 @@ namespace SchoolWebAPIApp.Controllers
 
             return Ok(studentsParents);
         }
+
+        [HttpGet("GetStudentParentByID/{ID}", Name = "GetStudentParentByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<studentParentDTO>>> GetByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            var studentParent = await clsStudentParents.GetByIDAsync(ID);
+
+            if (studentParent == null)
+                return NotFound($"No Student Parent With ID {ID}, Is Not Found!");
+
+
+            return Ok(studentParent.sParentDTO);
+        }
+
+        [HttpGet("IsStudentParentExistsByID/{ID}", Name = "IsStudentParentExistsByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<bool>>> IsStudentParentExistsByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            if (!await clsStudentParents.IsExistsAsync(ID))
+                return NotFound($"No Student Parent With ID {ID} Has Ben  Found!");
+
+
+            return Ok(true);
+        }
+
+        [HttpDelete("DeleteStudentParentByID/{ID}", Name = "DeleteStudentParentByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<bool>>> DeleteStudentParentByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            if (!await clsStudentParents.IsExistsAsync(ID))
+                return NotFound($"No Student Parent With ID {ID} Has Ben  Found!");
+
+
+            if (await clsStudentParents.DeleteAsync(ID))
+                return Ok($"Success, Student Parent With ID {ID} Has Ben Deleted.");
+            else
+                return StatusCode(StatusCodes.Status500InternalServerError);
+        }
     }
 }

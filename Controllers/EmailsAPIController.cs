08cc8700f5cfcb4f9ecbe71f8f52c2c0ce25cf20
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-05 23:02:36 +0100

    Create The Emails Empthy Classes In the Three Layers

---
+++ Controllers/EmailsAPIController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolWebAPIApp.Controllers
+{
+    [Route("api/EmailsAPI")]
+    [ApiController]
+    public class EmailsAPIController : ControllerBase
+    {
+    }
+}

5db427884561d9dbca6d514aca9996b3f9d15eaf
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-08 16:12:57 +0100

    Fixing Stored Procedures Prameters In SQL Server Add Two Methods To Three Layers Classess (Add New - Update) ✅ Test Them With Swagger : Result --> Success ✅

--- Controllers/EmailsAPIController.cs
+++ Controllers/EmailsAPIController.cs
@@ -76,5 +76,73 @@ namespace SchoolWebAPIApp.Controllers
             else
                 return StatusCode(StatusCodes.Status500InternalServerError);
         }
+
+        [HttpPost("AddNewEmail", Name = "AddNewEmail")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<classDTO>> AddNewEMailAsync(EmailDTO emailDTO)
+        {
+            if (emailDTO == null)
+            {
+                return BadRequest("DTO Is Null!");
+            }
+
+            if (emailDTO.PersonID < 0 || emailDTO.EmailTypeID <= 0 || string.IsNullOrEmpty(emailDTO.Address))
+            {
+                return base.BadRequest(" Some DTO Properties Are Empty!");
+            }
+
+            var email = new clsEmails(emailDTO, clsEmails.enMode.AddNew);
+
+            if (await email.SaveAsync())
+            {
+                return CreatedAtRoute("GetEmailByID", new { ID = email.ID }, email.emailDTO);
+            }
+            else
+            {
+                return StatusCode(500, new { Message = "Error, EMail Was Not Save." });
+            }
+
+        }
+
+        [HttpPut("UpdateEmail/{ID}", Name = "UpdateEmail")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<classDTO>> UpdateEMailAsync(int ID, EmailDTO emailDTO)
+        {
+            if (emailDTO == null)
+            {
+                return BadRequest("DTO Is Null!");
+            }
+
+
+            if (ID <= 0 || emailDTO.PersonID < 0 || emailDTO.EmailTypeID <= 0 || string.IsNullOrEmpty(emailDTO.Address))
+            {
+                return base.BadRequest(" Some DTO Properties Are Empty!");
+            }
+
+            var email = await clsEmails.GetByIDAsync(ID);
+
+            if (email == null)
+                return NotFound($"No email With {ID} Have Ben Found");
+
+            email.Address = emailDTO.Address;
+            email.PersonID = emailDTO.PersonID;
+            email.EmailTypeID = emailDTO.EmailTypeID;
+            email.IsPrimary = emailDTO.IsPrimary;
+
+            if (await email.SaveAsync())
+            {
+                return Ok($"Success, email With ID {email.ID} Has Ben Updated Successfully.");
+            }
+            else
+            {
+                return StatusCode(500, new { Message = "Error, email Was Not Save." });
+            }
+
+        }
     }
 }

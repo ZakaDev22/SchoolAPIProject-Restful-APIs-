331cdfafc0082b17ad554ec34e1731301ce6b0f6
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-08 23:17:18 +0100

    Fixing Stored Procedurs Parameters In Sql Server ✅ Add Two Methods In Three Layers (Get All - Get By ID) ✅ Test With Swagger : Success ✅

--- Controllers/PhoneNumbersAPIController.cs
+++ Controllers/PhoneNumbersAPIController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SchoolAPiDataAccessLayer;
+using SchoolBusinessLayer;
 
 namespace SchoolWebAPIApp.Controllers
 {
@@ -6,6 +9,35 @@ namespace SchoolWebAPIApp.Controllers
     [ApiController]
     public class PhoneNumbersAPIController : ControllerBase
     {
+        [HttpGet("GetAllPhoneNumbers", Name = "GetAllPhoneNumbers")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<phoneNumberDTO>>> GetAllPhoneNumbersAsync()
+        {
+            var numbers = await clsPhoneNumbers.GetAllAsync();
 
+            if (numbers.IsNullOrEmpty())
+                return NotFound("There Is No Data!");
+
+            return Ok(numbers);
+        }
+
+        [HttpGet("GetPhoneNumberByID/{ID}", Name = "GetPhoneNumberByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<phoneNumberDTO>>> GetPhoneNumberByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            var number = await clsPhoneNumbers.GetByIDAsync(ID);
+
+            if (number == null)
+                return NotFound($"No number With ID {ID} Is Not Found!");
+
+
+            return Ok(number.phoneNumberDTO);
+        }
     }
 }

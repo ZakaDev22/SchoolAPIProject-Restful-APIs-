aac9e9b2751aac3984d583492bdc985866eb7c65
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-08 15:24:38 +0100

    Create The Three Clases And Stating On Data Access First

--- Controllers/EmailsAPIController.cs
+++ Controllers/EmailsAPIController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SchoolAPiDataAccessLayer;
+using SchoolBusinessLayer;
 
 namespace SchoolWebAPIApp.Controllers
 {
@@ -6,5 +9,17 @@ namespace SchoolWebAPIApp.Controllers
     [ApiController]
     public class EmailsAPIController : ControllerBase
     {
+        [HttpGet("GetAllEmails", Name = "GetAllEmails")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<classDTO>>> GetAllEmailsAsync()
+        {
+            var classes = await clsClasses.GetAllAsync();
+
+            if (classes.IsNullOrEmpty())
+                return NotFound("There Is No Data!");
+
+            return Ok(classes);
+        }
     }
 }

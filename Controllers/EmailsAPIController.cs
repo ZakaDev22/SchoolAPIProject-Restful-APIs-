b49b971fff69929742e4aca3866bb2ca5e6d0a55
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-08 15:44:41 +0100

    Add Get All And Get By ID In The Three Layers ✅ Test The Methods With Swagger : Result --> Success ✅

--- Controllers/EmailsAPIController.cs
+++ Controllers/EmailsAPIController.cs
@@ -12,14 +12,32 @@ namespace SchoolWebAPIApp.Controllers
         [HttpGet("GetAllEmails", Name = "GetAllEmails")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<IEnumerable<classDTO>>> GetAllEmailsAsync()
+        public async Task<ActionResult<IEnumerable<EmailDTO>>> GetAllEmailsAsync()
         {
-            var classes = await clsClasses.GetAllAsync();
+            var emails = await clsEmails.GetAllAsync();
 
-            if (classes.IsNullOrEmpty())
+            if (emails.IsNullOrEmpty())
                 return NotFound("There Is No Data!");
 
-            return Ok(classes);
+            return Ok(emails);
+        }
+
+        [HttpGet("GetEmailByID/{ID}", Name = "GetEmailByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<classDTO>>> GetEmailByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            var email = await clsEmails.GetByIDAsync(ID);
+
+            if (email == null)
+                return NotFound($"No email With ID {ID} Is Not Found!");
+
+
+            return Ok(email.emailDTO);
         }
     }
 }

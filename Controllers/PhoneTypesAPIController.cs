92e785f58134bae2d48d4d867e6c48f081d90e35
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-13 23:11:05 +0100

    Add Two Phone Types Methods In All layers (Get All - Get By ID) - Swagger Tests : Success ✅

--- Controllers/PhoneTypesAPIController.cs
+++ Controllers/PhoneTypesAPIController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SchoolAPiDataAccessLayer;
+using SchoolBusinessLayer;
 
 namespace SchoolWebAPIApp.Controllers
 {
@@ -6,5 +9,35 @@ namespace SchoolWebAPIApp.Controllers
     [ApiController]
     public class PhoneTypesAPIController : ControllerBase
     {
+        [HttpGet("GetAllPhoneTypes", Name = "GetAllPhoneTypes")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<phoneTypesDTO>>> GetAllPhoneTypesAsync()
+        {
+            var phoneTypes = await clsPhoneTypes.GetAllAsync();
+
+            if (phoneTypes.IsNullOrEmpty())
+                return NotFound("There Is No Data!");
+
+            return Ok(phoneTypes);
+        }
+
+        [HttpGet("GetPhoneTypeByID/{ID}", Name = "GetPhoneTypeByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<phoneTypesDTO>>> GetPhoneTypeByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            var phoneType = await clsPhoneTypes.GetByIDAsync(ID);
+
+            if (phoneType is null)
+                return NotFound($"No phone Type With ID {ID} Is Not Found!");
+
+
+            return Ok(phoneType.phoneTypeDTO);
+        }
     }
 }

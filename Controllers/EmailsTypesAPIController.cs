e9051c178455c897290277a3e945d95e274494d6
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-13 22:53:10 +0100

    Add Two API Methods (Add new - Update) Swagger Test : Success All The Methods Works

--- Controllers/EmailsTypesAPIController.cs
+++ Controllers/EmailsTypesAPIController.cs
@@ -34,7 +34,7 @@ namespace SchoolWebAPIApp.Controllers
             var emailType = await clsEmailsTypes.GetByIDAsync(ID);
 
             if (emailType is null)
-                return NotFound($"No email Type With ID {ID} Is Not Found!");
+                return NotFound($"No type Type With ID {ID} Is Not Found!");
 
 
             return Ok(emailType.emailTypeDTO);
@@ -60,6 +60,72 @@ namespace SchoolWebAPIApp.Controllers
                 return Ok($"Success, EMail Type With ID {ID} Has Ben Deleted.");
             else
                 return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        [HttpPost("AddNewEmailType", Name = "AddNewEmailType")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<emailTypesDTO>> AddNewEMailTypeAsync(emailTypesDTO emailDTO)
+        {
+            if (emailDTO == null)
+            {
+                return BadRequest("DTO Is Null!");
+            }
+
+            if (string.IsNullOrEmpty(emailDTO.Name))
+            {
+                return base.BadRequest(" Some DTO Properties Are Empty!");
+            }
+
+            var type = new clsEmailsTypes(emailDTO, clsEmailsTypes.enMode.AddNew);
+
+            if (await type.SaveAsync())
+            {
+                return CreatedAtRoute("GetEmailTypeByID", new { ID = type.ID }, type.emailTypeDTO);
+            }
+            else
+            {
+                return StatusCode(500, new { Message = "Error, EMail Type Was Not Save." });
+            }
+
+        }
+
+        [HttpPut("UpdateEmailType/{ID}", Name = "UpdateEmailType")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<EmailDTO>> UpdateEmailTypeAsync(int ID, emailTypesDTO emailDTO)
+        {
+            if (emailDTO == null)
+            {
+                return BadRequest("DTO Is Null!");
+            }
+
+
+            if (string.IsNullOrEmpty(emailDTO.Name))
+            {
+                return BadRequest(" Some DTO Properties Are Empty!");
+            }
+
+            var emailType = await clsEmailsTypes.GetByIDAsync(ID);
+
+            if (emailType is null)
+                return NotFound($"No type With {ID} Have Ben Found");
+
+            emailType.Name = emailDTO.Name;
+
+
+            if (await emailType.SaveAsync())
+            {
+                return Ok($"Success, type With ID {emailType.ID} Has Ben Updated Successfully.");
+            }
+            else
+            {
+                return StatusCode(500, new { Message = "Error, type Was Not Save." });
+            }
+
         }
     }
 }

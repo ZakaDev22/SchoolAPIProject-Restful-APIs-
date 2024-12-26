de0a24d7e1b95d0500fe22b939152f1bc31bd8a8
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-26 21:40:37 +0100

    Create Two New Method In Each Layer (Get All - Get By Id) Swagger Test : Success ✅

--- Controllers/StudentGradesAPIController.cs
+++ Controllers/StudentGradesAPIController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SchoolAPiDataAccessLayer;
+using SchoolBusinessLayer;
 
 namespace SchoolWebAPIApp.Controllers
 {
@@ -6,5 +9,35 @@ namespace SchoolWebAPIApp.Controllers
     [ApiController]
     public class StudentGradesAPIController : ControllerBase
     {
+        [HttpGet("GetAllStudentsGrades", Name = "GetAllStudentsGrades")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<sGradeDTO>>> GetAllStudentsGradesAsync()
+        {
+            var sGrades = await clsStudentGrades.GetAllAsync();
+
+            if (sGrades.IsNullOrEmpty())
+                return NotFound("There Is No Data!");
+
+            return Ok(sGrades);
+        }
+
+        [HttpGet("GetStudentGradeByID/{ID}", Name = "GetStudentGradeByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<sGradeDTO>>> GetStudentGradeByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            var sGrade = await clsStudentGrades.GetByIDAsync(ID);
+
+            if (sGrade == null)
+                return NotFound($"No Student Grade With ID {ID} Is Not Found!");
+
+
+            return Ok(sGrade.sGradeDTO);
+        }
     }
 }

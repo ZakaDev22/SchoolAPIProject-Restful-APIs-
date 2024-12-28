7c5de64dbb5acc511f4734842db9a6d1e8113f99
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-28 16:03:12 +0100

    Add Only Get All Departments And Get By ID In Three Layers

--- Controllers/DepartmentsAPIController.cs
+++ Controllers/DepartmentsAPIController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SchoolAPiDataAccessLayer;
+using SchoolBusinessLayer;
 
 namespace SchoolWebAPIApp.Controllers
 {
@@ -6,5 +9,35 @@ namespace SchoolWebAPIApp.Controllers
     [ApiController]
     public class DepartmentsAPIController : ControllerBase
     {
+        [HttpGet("GetAllDepartments", Name = "GetAllDepartments")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<EmailDTO>>> GetAllDepartmentsAsync()
+        {
+            var departments = await clsDepartments.GetAllAsync();
+
+            if (departments.IsNullOrEmpty())
+                return NotFound("There Is No Data!");
+
+            return Ok(departments);
+        }
+
+        [HttpGet("GetDepartmentByID/{ID}", Name = "GetDepartmentByID")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<EmailDTO>>> GetDepartmentByIDAsync(int ID)
+        {
+            if (ID <= 0)
+                return BadRequest($"Invalid ID !");
+
+            var department = await clsDepartments.GetByIDAsync(ID);
+
+            if (department == null)
+                return NotFound($"No department With ID {ID} Is Not Found!");
+
+
+            return Ok(department.departmentDTO);
+        }
     }
 }

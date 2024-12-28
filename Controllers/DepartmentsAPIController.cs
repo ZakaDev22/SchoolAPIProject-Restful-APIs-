9b14db4f59695ea9ffa88a95aa6d9ae03100e38b
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-28 15:34:51 +0100

    Add Empty Departments Classes In The Three Layers

---
+++ Controllers/DepartmentsAPIController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolWebAPIApp.Controllers
+{
+    [Route("api/DepartmentsAPI")]
+    [ApiController]
+    public class DepartmentsAPIController : ControllerBase
+    {
+    }
+}

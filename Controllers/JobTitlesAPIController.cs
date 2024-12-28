07ace5b2e162d405563d45e57e361b0a36de44db
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-28 21:26:43 +0100

    Add JobTitles Class Files In The Three Layers

---
+++ Controllers/JobTitlesAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolWebAPIApp.Controllers
+{
+    [Route("api/JobTitlesAPI")]
+    [ApiController]
+    public class JobTitlesAPIController : ControllerBase
+    {
+
+    }
+}
